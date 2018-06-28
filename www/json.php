c7f02ea58712f548e752f196d3831b2af3581c83
Adham Hayukalbu <sojoy.enjoyaja@gmail.com>
2018-06-28 17:43:53 +0700

    Update JSON

--- www/json.php
+++ www/json.php
@@ -2,7 +2,7 @@
 <?php
 include "db.php";
 $data=array();
-$q=mysqli_query($con,"select * from `course_details`");
+$q=mysqli_query($con,"select * from course_details");
 while ($row=mysqli_fetch_object($q)){
  $data[]=$row;
 }
